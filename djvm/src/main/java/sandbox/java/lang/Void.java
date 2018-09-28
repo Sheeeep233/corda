0fb380718d0e92b44dec57063d14d68d13e6b4fb
Chris Rankin <chris.rankin@r3.com>
2018-09-28 17:32:07 +0100

    WIP - sandbox classloading

---
+++ djvm/src/main/java/sandbox/java/lang/Void.java
@@ -0,0 +1,9 @@
+package sandbox.java.lang;
+
+@SuppressWarnings("unused")
+public final class Void extends Object {
+    @SuppressWarnings("unchecked")
+    public static final Class<Void> TYPE = (Class) java.lang.Void.TYPE;
+
+    private Void() {}
+}
